1fa729821b67b71f6f18594392f5fb780d1713e8
Anastasiia <101875515+Annix48@users.noreply.github.com>
2022-09-06 18:50:20 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,45 +2,44 @@ import '../css/common.css';
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-
-player.on('timeupdate', throttle(saveTime, 1000));
-
-function saveTime({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
-}
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-
-// const TIME_KEY = 'videoplayer-current-time';
 // const iframe = document.querySelector('iframe');
 // const player = new Vimeo.Player(iframe);
 
-// const onPlay = function (data) {
-//   const strigifyData = JSON.stringify(data);
-//   localStorage.setItem(TIME_KEY, strigifyData);
-// };
-// player.on('timeupdate', throttle(onPlay, 1000));
+// player.on('timeupdate', throttle(saveTime, 1000));
 
-// function resumePlayback() {
-//   if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
-//     return;
-//   }
-//   const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
-//   if (paused) {
-//     player
-//       .setCurrentTime(paused)
-//       .then(function (seconds) {})
-//       .catch(function (error) {
-//         switch (error.name) {
-//           case 'Error':
-//             break;
-//           default:
-//             break;
-//         }
-//       });
-//   }
+// function saveTime({ seconds }) {
+//   localStorage.setItem('videoplayer-current-time', seconds);
 // }
-// resumePlayback();
+
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+const TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+  const strigifyData = JSON.stringify(data);
+  localStorage.setItem(TIME_KEY, strigifyData);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function resumePlayback() {
+  if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
+    return;
+  }
+  const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
+  if (paused) {
+    player
+      .setCurrentTime(paused)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'Error':
+            break;
+          default:
+            break;
+        }
+      });
+  }
+}
+resumePlayback();
